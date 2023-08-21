b63e147a05791a21ea48dc852f5d524ebdfac6dd
Pavel Veremeienko <pavel.veremeienko@gmail.com>
2023-08-21 12:13:32 +0300

    Add project files.

---
+++ Fibonacci/FibonacciCounter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    internal static class FibonacciCounter
+    {
+        public static ulong GetFibonacciNumber(uint index) //O(n)
+        {
+            if (index == 0)
+            {
+                throw new ArgumentException("Must be more than 0", nameof(index));
+            }
+
+            ulong second = 1, first = 0;
+
+            if (index == 1) 
+            { 
+                return first;
+            }
+
+            for (uint i = 2; i < index; i++) // O^n
+            {
+                ulong temp = second;
+                second += first;
+                first = temp;
+            }
+
+            return second;
+        }
+
+        public static ulong GetFibonacciNumberRecursive(uint index) // O(2^n)
+        {
+            if (index == 0)
+            {
+                throw new ArgumentException("Must be more than 0", nameof(index));
+            }
+
+            if (index == 1)
+            {
+                return 0;
+            }
+            else if (index == 2)
+            {
+                return 1;
+            }
+            else
+            {
+                return GetFibonacciNumberRecursive(index - 1) + GetFibonacciNumberRecursive(index - 2);
+            }
+        }
+    }
+}
---
+++ Fibonacci/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Fibonacci;
+
+Console.WriteLine("Hello, World!");
+uint i = 42;
+Console.WriteLine($"{i}th Fibonacci number = {FibonacciCounter.GetFibonacciNumber(i)}");
+Console.WriteLine($"{i}th Fibonacci number recursive = {FibonacciCounter.GetFibonacciNumberRecursive(i)}");
